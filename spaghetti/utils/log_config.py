4c25c90f8e5017408409f84f1a17f236f5f9733d
Ngene <hplxg@hotmail.com>
2025-06-29 18:21:50 +0100

    feat: refactoring for logging

--- spaghetti/utils/log_config.py
+++ spaghetti/utils/log_config.py
@@ -20,7 +20,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
 # Define the formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 # file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
